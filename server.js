adb1723a4d1d692a167383d6f4f0afaa64818003
ionutgabrielalexa <aligaby2000@yahoo.com>
2022-06-14 14:47:12 +0300

    Connected to DB

--- server.js
+++ server.js
@@ -2,5 +2,7 @@ import app from "./src/app.js";
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
-  console.log(`The server is running on port ${port}`);
+  console.log(
+    `The server is running on port ${port}. Look in the browser to see my Nodemon API.`,
+  );
 });
--- src/app.js
+++ src/app.js
@@ -1,6 +1,24 @@
 import express from "express";
+import mongoose from "mongoose";
 import { accessDb } from "../config/dbConnect.js";
+import Book from "../models/bookModel.js";
 
 const app = express();
+const db = mongoose.connect(accessDb.MONGO_URI, {dbName: accessDb.MONGO_DB})
+const bookRouter = express.Router();
+
+bookRouter.route('/books').get((req, res) => {
+  Book.find((err, books) => {
+    if (err) {
+      return res.send(err);
+    }
+    return res.json(books);
+  });
+});
+app.use('/api', bookRouter);
+
+app.get("/", (req, res) => {
+  res.send(`Welcome to my Nodemon API!`);
+});
 
 export default app;
