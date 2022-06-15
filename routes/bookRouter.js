0f31dd10a94c1804eead2272a714d4b1436fc3ce
ionutgabrielalexa <aligaby2000@yahoo.com>
2022-06-15 11:37:12 +0300

    Create PATCH

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -4,65 +4,96 @@ import Book from "../models/bookModel.js";
 function routes() {
   const bookRouter = express.Router();
 
-  bookRouter.route('/books')
-  .get((req, res) => {
+  bookRouter.use("/books", (req, res, next) => {
     Book.find((err, books) => {
       if (err) {
         return res.send(err);
       }
-      return res.json(books);
-    });
-  });
-  
-  bookRouter.route('/books/:bookId')
-  .get((req, res) => {
-    Book.findById(req.params.bookId, (err, book) => {
-      if (err) {
-        return res.send(err);
+      if (books) {
+        req.book = books;
+        return next();
       }
-      return res.status(201).json(book);
-    });
-  });
-  
-  bookRouter.route('/books')
-  .post((req, res) => {
-    const book = new Book(req.body);
-    book.save();
-  
-    return res.json(book);
-  })
-  .get((req, res) => {
-    Book.find((err, books) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(books);
+      // return res.json(books);
     });
   });
 
-  bookRouter.route('/books/:bookId')
-  .get((req, res) => {
-    Book.findById(req.params.bookId, (err, book) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.status(201).json(book);
+  bookRouter.route("/books").get((req, res) => {
+    return res.json(req.book);
+    // Book.find((err, books) => {
+    //   if (err) {
+    //     return res.send(err);
+    //   }
+    //   return res.json(books);
+    // });
+  });
+
+  bookRouter
+    .route("/books")
+    .post((req, res) => {
+      const book = new Book(req.body);
+      book.save();
+
+      return res.json(book);
+    })
+    .get((req, res) => {
+      return res.json(req.book);
+      // Book.find((err, books) => {
+      //   if (err) {
+      //     return res.send(err);
+      //   }
+      //   return res.json(books);
+      // });
     });
-  })
-  .put((req, res) => {
+
+  bookRouter.use("/books/:bookId", (req, res, next) => {
     Book.findById(req.params.bookId, (err, book) => {
       if (err) {
         return res.send(err);
       }
+      if (book) {
+        req.book = book;
+        return next();
+      }
+      return res.sendStatus(404);
+    });
+  });
+
+  bookRouter
+    .route("/books/:bookId")
+    .get((req, res) => res.json(req.book))
+    .put((req, res) => {
+      const { book } = req;
       book.title = req.body.title;
       book.author = req.body.author;
       book.genre = req.body.genre;
       book.read = req.body.read;
-      book.save();
+      req.book.save((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      });
+    })
+    .patch((req, res) => {
+      const { book } = req;
 
-      return res.status(201).json(book);
+      if (req.body._id) {
+        delete req.body._id;
+      }
+
+      Object.entries(req.body).forEach((item) => {
+        const key = item[0];
+        const value = item[1];
+        book[key] = value;
+      });
+
+      req.book.save((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      });
     });
-  });
 
   return bookRouter;
 }
--- server.js
+++ server.js
@@ -2,17 +2,19 @@ import app from "./src/app.js";
 import mongoose from "mongoose";
 import { accessDb } from "./config/dbConnect.js";
 
-(async function() {
+(async function () {
   try {
     await mongoose.connect(accessDb.MONGO_URI, { dbName: accessDb.MONGO_DB });
 
     const port = process.env.PORT || 3000;
     app.listen(port, () => {
       console.log(
-        `The server is running on port ${port}. Look in the browser to see my Nodemon API.`,
-        );
-      });
-  } catch(err) {
-    throw new Error(`Connection on database is not established. This is the error\n${err}`)
+        `The server is running on port ${port}. Look my Nodemon API.`
+      );
+    });
+  } catch (err) {
+    throw new Error(
+      `Connection on database is not established. This is the error\n${err}`
+    );
   }
 })();
